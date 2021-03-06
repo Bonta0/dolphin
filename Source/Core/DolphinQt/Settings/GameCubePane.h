a47e5f0b2c0bb12924cae40f3f769e36d724ad3a
Bonta-kun <40473493+Bonta0@users.noreply.github.com>
2021-03-06 16:42:31 +0100

    Qt: GBA Config dialog

--- Source/Core/DolphinQt/Settings/GameCubePane.h
+++ Source/Core/DolphinQt/Settings/GameCubePane.h
@@ -8,6 +8,7 @@
 
 class QCheckBox;
 class QComboBox;
+class QLineEdit;
 class QPushButton;
 
 class GameCubePane : public QWidget
@@ -26,9 +27,20 @@ private:
   void UpdateButton(int slot);
   void OnConfigPressed(int slot);
 
+  void BrowseGBABios();
+  void SaveRomPathChanged();
+  void BrowseGBASaves();
+
   QCheckBox* m_skip_main_menu;
   QComboBox* m_language_combo;
 
   QPushButton* m_slot_buttons[3];
   QComboBox* m_slot_combos[3];
+
+  QCheckBox* m_gba_threads;
+  QCheckBox* m_gba_save_rom_path;
+  QPushButton* m_gba_browse_bios;
+  QLineEdit* m_gba_bios_edit;
+  QPushButton* m_gba_browse_saves;
+  QLineEdit* m_gba_saves_edit;
 };
